2b7245e551c84ac08eed6724632077d4c697b23f
Andrew <andreeww2@gmail.com>
2017-05-08 14:42:27 +0200

    Dodanie klasy Field

---
+++ database/mapper/Field.php
@@ -0,0 +1,33 @@
+<?php
+namespace database\mapper;
+
+class Field
+{
+    protected $name = null;
+    protected $operator = null;
+    protected $comps = [];
+    protected $incomplete = false;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function addTest($operator, $value)
+    {
+        $this->comps[] = array('name' => $this->name,
+                               'operator' => $operator,
+                               'value' => $value
+                              );
+    }
+
+    public function getComps()
+    {
+        return $this->comps;
+    }
+
+    public function isIncomplete()
+    {
+        return empty($this->comps);
+    }
+}
