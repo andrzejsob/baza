ce486f8c8d4c1d9f957ca4534a2de6814094a5c2
Andrew <andreeww2@gmail.com>
2017-05-10 22:06:43 +0200

    Dodanie klasy IdentityObject.php

---
+++ database/mapper/IdentityObject.php
@@ -0,0 +1,66 @@
+<?php
+namespace database\mapper;
+
+class IdentityObject
+{
+    protected $currentfield = null;
+    protected $fields = array();
+    private $and = null;
+    private $enforce = array();
+
+    public function __construct($field = null, array $enforce = null)
+    {
+        if (!is_null($enforce)) {
+            $this->enforce = $enforce;
+        }
+        if (!is_null($field)) {
+            $this->fields($field);
+        }
+    }
+
+    public function getObjectFields()
+    {
+        return $this->enforce;
+    }
+
+    public function field($fieldname)
+    {
+        if (isset($this->fields[$fieldname])) {
+            $this->currentfield = $this->fields[$fieldname];
+        } else {
+            $this->currentfield = new Field($fieldname);
+            $this->fields[$fieldname] = $this->currentfield;
+        }
+        return $this;
+    }
+
+    public function eq($value)
+    {
+        return $this->operator('=', $value);
+    }
+
+    public function lt($value)
+    {
+        return $this->operator('<', $value);
+    }
+
+    public function gt($value)
+    {
+        return $this->operator('>', $value);
+    }
+
+    public function operator($symbol, $value)
+    {
+        $this->currentfield->addTest($symbol, $value);
+        return $this;
+    }
+
+    public function getComps()
+    {
+        $comparisons = array();
+        foreach ($this->fields as $field) {
+            $comparisons = array_merge($field->getComps());
+        }
+        return $comparisons;
+    }
+}
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 ini_set('display_errors', 1);
-
 require "vendor/autoload.php";
+
+use database\mapper as mapper;
 //woo\controller\Controller::run();
-$mapper = new \database\mapper\VenueMapper();
+$mapper = new mapper\VenueMapper();
 $v_collection = $mapper->findAll();
 foreach($v_collection as $venue) {
     echo $venue->getId().' -> '.$venue->getName().'</br>';
 }
 print_r(Space::class);
+
+$iobj = new mapper\IdentityObject();
+$iobj->field('name')->gt('Coś tam')->lt('nowe');
+//$iobj->getComps();
+echo '<pre>';
+print_r($iobj->getComps());
+echo '</pre>';
